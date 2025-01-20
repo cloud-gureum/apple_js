90c57965206400737dfb2ccf15e21d3a636555d2
cloud <lilpark1024@gmail.com>
2025-01-20 22:30:27 +0900

    Day8

--- tab.js
+++ tab.js
@@ -23,3 +23,33 @@
 
         content.eq(index).addClass('show');
     }
+
+
+    var car = {name: '소나타', price: 50000, color: 'white'};
+    $('#car-name').html(car.name);
+    $('#car-price').html(car.price);
+
+
+var pants = [28, 30, 32];
+var shirts = [95, 100, 105];
+
+    $('.form-select').on('input', function(){
+        if($(this).val() == '셔츠'){
+            $('.form-select').eq(1).removeClass('form-hide');
+            $('.form-select').eq(1).html('');
+            shirts.forEach(function(a){
+                $('.form-select').eq(1).append(`<option>${a}</option>`)
+            })
+        }
+        else if($(this).val() == '바지'){
+            $('.form-select').eq(1).removeClass('form-hide');
+            $('.form-select').eq(1).html('');
+            pants.forEach(function(a){
+                $('.form-select').eq(1).append(`<option>${a}</option>`)
+            })
+        }
+        else{
+            $('.form-select').eq(1).addClass('form-hide');
+        }
+    })
+    
